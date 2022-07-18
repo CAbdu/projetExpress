45e92eac46b9fd9690bcaf9d0e6d36f590e434d0
CAbdu <abduchaib@hotmail.fr>
2022-07-18 06:11:05 +0200

    lien navbar fini

--- index.js
+++ index.js
@@ -14,4 +14,20 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
 res.render('main', {layout : 'index'})
 })
+
+app.get('/tournoi', (req, res) => {
+    res.render('tournoi', {layout : 'index'})
+})
+
+app.get('/chercheUneTeam', (req, res) => {
+    res.render('chercheUneTeam', {layout : 'index'})
+})
+
+app.get('/connection', (req, res) => {
+    res.render('connection', {layout : 'index'});
+})
+
+app.get('/inscription', (req, res) => {
+    res.render('inscription', {layout : 'index'});
+});
 app.listen(port, () => console.log(`App listening to port ${port}`))
